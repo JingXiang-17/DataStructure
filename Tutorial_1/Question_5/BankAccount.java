2221bc7f806dfa74d6dcbcb14dcee74d1396fc2b
Kor Jing Xiang <25005896@siswa.um.edu.my>
2026-03-17 23:23:09 +0800

    Tutorial 1

--- Tutorial_1/Question_5/BankAccount.java
+++ Tutorial_1/Question_5/BankAccount.java
@@ -1,5 +1,18 @@
 package Tutorial_1.Question_5;
 
-public class BankAccount {
-    
+public class BankAccount implements Account {
+    private int balance;
+
+    public BankAccount (int balance){
+        this.balance = balance;
+    }
+
+    public int deposit (int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public boolean withdraw (int amount) {
+        return (balance >= amount) ? (balance-=amount) >=0 : false ;
+    }
 }
